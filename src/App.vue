d8011ff0385871d9c4003f68e4eb6947e1dca43f
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-26 16:28:43 +0100

    [milestone4] done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
+<script>
+import axios from 'axios';
+export default {
+  name:'App',
+  data(){
+    return {
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      movies: [],
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + 'movies')
+      .then(result =>{
+        this.movies = result.data.movies;
+        console.log(this.movies);
+      })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
+}
+</script>
+
+
 <template>
   <h1>Main App Here</h1>
 </template>
 
-<script>
-export default {
-
-}
-</script>
 
 <style lang="scss">
 @use './styles/general.scss';
